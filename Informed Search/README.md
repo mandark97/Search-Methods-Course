b8e39a40284e7bc9ae7b63b385cb65f0fb92961e
Matei <melinte.matei@gmail.com>
2019-11-23 18:00:04 +0200

    add informed search algorithsm

---
+++ Informed Search/README.md
@@ -0,0 +1,22 @@
+# Informed Search Problem
+
+## Pathfinding in a maze
+
+Given a maze (represented as a matrix with 1 values being the walls), find the shortest path to reach the destination. 
+
+### Proposed Algorithms
+
+The general aproach for this problem is named **best-first search**. Best-first search is an instance of the GRAPH-SEARCH algorithm in which a node is selected for expansion based on an **evaluation function** *f(n)*. The node with the lowest evaluation is expanded first. Most best-first algorithms include an component names **heuristic function** *h(n)*.
+
+Based on the chosen **evaluation function** we present two algorithms:
+
+- Greedy best-first search
+- A* search
+
+#### Greedy best-first search
+
+**Greedy best-first search** tries to expand the node that is closest to the goal. In this case *f(n) = h(n)*. This algorithm doesn't guarantee the optimal solution.
+
+#### A* search
+
+**A\* search** evaluates nodes by combining **the cost to reach the node** *g(n)*, and *h(n)* the estimated cost of the cheapest path. The **evaluation function** is *f(n) = h(n) + g(n)*. This algorithm finds the optimal solution.
--- Informed Search/src/main.py
+++ Informed Search/src/main.py
@@ -13,26 +13,78 @@ class Node(object):
     def move(self, movement):
         return Node(self, (self.position[0] + movement[0], self.position[1] + movement[1]))
 
+    # check if is in maze's boundries and is not a wall
     def is_valid(self, maze):
-        if self.position[0] > (len(maze) - 1) or \
-                self.position[0] < 0 or \
-                self.position[1] > (len(maze[len(maze) - 1]) - 1) or \
-                self.position[1] < 0 or \
-                maze[self.position[0]][self.position[1]] != 0:
-            return False
+        return self.position[0] < len(maze) and \
+            self.position[0] >= 0 and \
+            self.position[1] < len(maze[0]) and \
+            self.position[1] >= 0 and \
+            maze[self.position[0]][self.position[1]] == 0
 
-MOVEMENTS = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-def astar(maze, start, end):
-    start_node = Node(None, start)
-    end_node = Node(None, end)
 
-    open_list = []
-    closed_list = []
+MOVEMENTS = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+# MOVEMENTS = [(0, -1), (0, 1), (-1, 0), (1, 0),
+#              (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
-    open_list.append(start_node)
 
-    while(len(open_list) > 0):
+class InformedSearch(object):
+    def __init__(self, maze, start, end):
+        if maze[start[0]][start[1]] == 1:
+            raise "Invalid start"
 
+        if maze[end[0]][end[1]] == 1:
+            raise "Invalid end"
+
+        self.start_node = Node(None, start)
+        self.end_node = Node(None, end)
+        self.maze = maze
+
+    def run(self, mode="greedy"):
+        open_list = []
+        closed_list = []
+        open_list.append(self.start_node)
+
+        while(len(open_list) > 0):
+            current_node, current_index = self.__select_current_node(open_list)
+            open_list.pop(current_index)
+            closed_list.append(current_node)
+
+            if current_node == self.end_node:
+                return self.__return_path(current_node)
+
+            for child in self.__compute_children(current_node):
+                if child in closed_list:
+                    continue
+
+                if mode == "greedy":
+                    self.__greedy_update(child, current_node)
+                elif mode == "a_star":
+                    self.__a_star_update(child, current_node)
+
+                if any([child == open_node and child.g > open_node.g for open_node in open_list]):
+                    continue
+
+                open_list.append(child)
+
+    def __greedy_update(self, child, current_node):
+        child.g = current_node.g + 1
+        child.h = self.__compute_h(child, self.end_node)
+        child.f = child.h
+
+    def __a_star_update(self, child, current_node):
+        child.g = current_node.g + 1
+        child.h = self.__compute_h(child, self.end_node)
+        child.f = child.g + child.h
+
+    def __return_path(self, node):
+        path = []
+        while node is not None:
+            path.append(node.position)
+            node = node.parent
+
+        return path[::-1]
+
+    def __select_current_node(self, open_list):
         current_node = open_list[0]
         current_index = 0
 
@@ -41,38 +93,42 @@ def astar(maze, start, end):
                 current_node = item
                 current_index = index
 
-        open_list.pop(current_index)
-        closed_list.append(current_node)
+        return current_node, current_index
 
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
+    def __compute_h(self, node, end_node):
+        return ((node.position[0] - end_node.position[0]) ** 2) + ((node.position[1] - end_node.position[1]) ** 2)
 
-            return path[::-1]
-
+    def __compute_children(self, current_node):
         children = []
 
         # Adjacent squares
         for new_position in MOVEMENTS:
             new_node = current_node.move(new_position)
 
-            if new_node.is_valid(maze):
+            if new_node.is_valid(self.maze):
                 children.append(new_node)
 
-        for child in children:
-            if child in closed_list:
-                continue
+        return children
 
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) **
-                       2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
 
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
+def main():
+    maze = [[0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-            open_list.append(child)
+    start = (0, 0)
+    end = (7, 6)
+
+    path = InformedSearch(maze, start, end).run(mode="a_star")
+    print(path)
+
+
+if __name__ == '__main__':
+    main()
